cc37db499792478375b21a64900c3eaccfe76497
kelin-xycs <kelin_xycs@163.com>
2018-09-10 12:54:37 +0800

    Signed-off-by: kelin-xycs <kelin_xycs@163.com>

---
+++ TcpTextListener/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace TcpTextListener
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnShowText = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(14, 57);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(109, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "开始监听";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(44, 20);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(100, 21);
+            this.txtIP.TabIndex = 1;
+            this.txtIP.Text = "127.0.0.1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(205, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "端口";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(251, 20);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 21);
+            this.txtPort.TabIndex = 3;
+            this.txtPort.Text = "9000";
+            // 
+            // btnShowText
+            // 
+            this.btnShowText.Location = new System.Drawing.Point(140, 57);
+            this.btnShowText.Name = "btnShowText";
+            this.btnShowText.Size = new System.Drawing.Size(109, 23);
+            this.btnShowText.TabIndex = 5;
+            this.btnShowText.Text = "显示文本";
+            this.btnShowText.UseVisualStyleBackColor = true;
+            this.btnShowText.Click += new System.EventHandler(this.btnShowText_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(12, 96);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMsg.Size = new System.Drawing.Size(691, 346);
+            this.txtMsg.TabIndex = 6;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(270, 57);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(340, 23);
+            this.btnTest.TabIndex = 7;
+            this.btnTest.Text = "测测看  点击这个按钮会发送一个 Http 请求 给自己";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(628, 57);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "清空文本";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 454);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.btnShowText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnShowText;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ TcpTextListener/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+
+using System.Threading;
+using System.IO;
+
+namespace TcpTextListener
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Socket serverSocket;
+        private MemoryStream stream = new MemoryStream();
+        private object lockObj = new object();
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            if (serverSocket != null)
+            {
+                WriteMsg("已启动监听 。");
+                return;
+            }
+
+            try
+            {
+                string ipStr = txtIP.Text;
+                int port = int.Parse(txtPort.Text);
+
+                IPAddress ip = IPAddress.Parse(ipStr);
+                serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                serverSocket.Bind(new IPEndPoint(ip, port));  //绑定IP地址：端口
+                serverSocket.Listen(10);    //设定最多10个排队连接请求
+
+                WriteMsg("启动监听 " + serverSocket.LocalEndPoint.ToString() + " 成功 。");
+
+                //通过Clientsoket发送数据
+                Thread thread = new Thread(ListenClientConnect);
+                thread.IsBackground = true;
+                thread.Start();
+            }
+            catch(Exception ex)
+            {
+                WriteMsg(ex.ToString());
+            }
+        }
+
+        private void ListenClientConnect()
+        {
+            while (true)
+            {
+                Socket clientSocket = serverSocket.Accept();
+                
+                Thread thread = new Thread(ReceiveMessage);
+                thread.IsBackground = true;
+                thread.Start(clientSocket);
+            }
+        }
+
+        private void ReceiveMessage(object clientSocket)
+        {
+            Socket myClientSocket = (Socket)clientSocket;
+
+            byte[] b = new byte[1024];
+
+            while (true)
+            {
+                try
+                {
+                    //通过clientSocket接收数据
+                    int receiveCount = myClientSocket.Receive(b);
+
+                    //  receiveCount == 0 表示 客户端 连接 已关闭
+                    if (receiveCount == 0)
+                    {
+                        break;
+                    }
+
+                    lock (lockObj)
+                    {
+                        stream.Write(b, 0, receiveCount);
+                    }
+
+                    if (receiveCount > 0)
+                    {
+                        this.Invoke(new Action<string>(WriteMsg), "接收到 " + receiveCount + " 个 Byte 的数据 。");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    this.Invoke(new Action<string>(WriteMsg), "ReceiveMessage Error: " + ex.ToString());
+                    myClientSocket.Shutdown(SocketShutdown.Both);
+                    myClientSocket.Close();
+                    break;
+                }
+            }
+        }
+
+
+        private void btnShowText_Click(object sender, EventArgs e)
+        {            
+            string s;
+
+            lock (lockObj)
+            {
+                stream.Position = 0;
+
+                StreamReader sr = new StreamReader(stream);
+
+                s = sr.ReadToEnd();
+
+                stream.Dispose();
+                stream = new MemoryStream();
+            }
+
+            WriteMsg("接收到的文本是 ：" + s);   
+        }
+
+        private void WriteMsg(string msg)
+        {
+            txtMsg.AppendText(DateTime.Now.ToString("HH:mm:ss") + "\r\n" + msg + "\r\n\r\n");
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string ipStr = txtIP.Text;
+                int port = int.Parse(txtPort.Text);
+
+                IPAddress ip = IPAddress.Parse(ipStr);
+                Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                s.Connect(new IPEndPoint(ip, port)); //配置服务器IP与端口
+
+                s.Send(Encoding.UTF8.GetBytes("Hello Server !  This a test message from client ."));
+
+                s.Dispose();
+            }
+            catch(Exception ex)
+            {
+                WriteMsg(ex.ToString());
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtMsg.Clear();
+        }
+    }
+}
